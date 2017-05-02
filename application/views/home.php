504604b52a55a69737e7cd9252f6fea05efcecdd
rumi <mujtaba.rumi1@gmail.com>
2017-05-02 17:56:09 +0600

    rumi

--- application/views/home.php
+++ application/views/home.php
@@ -628,7 +628,7 @@
                 var btn = $(x).data('panel-id');
                 var p_id=document.getElementById('product_id').value;
                 var x = parseInt(document.getElementById(btn).value);
-                alert(x);
+                //alert(x);
                 var newx= x+1;
 //                document.getElementById('btn').value = newx;
                 $.ajax({
